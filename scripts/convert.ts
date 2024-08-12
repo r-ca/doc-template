667111bd63470cb272a5f6cdb6a5390f06c29952
rca <nem@nem.x0.to>
2024-08-12 17:25:57 +0900

    cjsに対応

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -1,18 +1,23 @@
-import PuppeteerHTMLPDF from "puppeteer-html-pdf";
+import PuppeteerHTMLPDF from 'puppeteer-html-pdf';
 
-const htmlPdf = new PuppeteerHTMLPDF();
+async function generatePDF() {
+  const htmlPdf = new PuppeteerHTMLPDF();
 
-htmlPdf.setOptions({
+  htmlPdf.setOptions({
     format: "A4",
-});
+  });
 
-const html = await htmlPdf.readFile(`${__dirname}/dist/all.html`, "utf8");
+  try {
+    const html = await htmlPdf.readFile(`${__dirname}/../dist/all.html`, "utf8");
 
-try {
     const pdfBuffer = await htmlPdf.create(html);
-    const path = `${__dirname}/dist/result.pdf`;
+    const path = `${__dirname}/../dist/result.pdf`;
     await htmlPdf.writeFile(pdfBuffer, path);
+    
     console.log("PDF created successfully");
-} catch (error) {
+  } catch (error) {
     console.error("Error creating PDF", error);
+  }
 }
+
+generatePDF();
