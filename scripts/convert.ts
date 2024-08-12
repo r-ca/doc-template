5ba02f1054e8fc72baa99faac4f38f231251acb4
rca <nem@nem.x0.to>
2024-08-12 20:59:23 +0900

    マージン追加

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -1,23 +1,29 @@
 import PuppeteerHTMLPDF from 'puppeteer-html-pdf';
 
 async function generatePDF() {
-  const htmlPdf = new PuppeteerHTMLPDF();
+    const htmlPdf = new PuppeteerHTMLPDF();
 
-  htmlPdf.setOptions({
-    format: "A4",
-  });
+    htmlPdf.setOptions({
+        format: "A4",
+        margin: {
+            top: "20mm",
+            right: "20mm",
+            bottom: "20mm",
+            left: "20mm"
+        },
+    });
 
-  try {
-    const html = await htmlPdf.readFile(`${__dirname}/../dist/all.html`, "utf8");
+    try {
+        const html = await htmlPdf.readFile(`${__dirname}/../dist/all.html`, "utf8");
 
-    const pdfBuffer = await htmlPdf.create(html);
-    const path = `${__dirname}/../dist/result.pdf`;
-    await htmlPdf.writeFile(pdfBuffer, path);
-    
-    console.log("PDF created successfully");
-  } catch (error) {
-    console.error("Error creating PDF", error);
-  }
+        const pdfBuffer = await htmlPdf.create(html);
+        const path = `${__dirname}/../dist/result.pdf`;
+        await htmlPdf.writeFile(pdfBuffer, path);
+
+        console.log("PDF created successfully");
+    } catch (error) {
+        console.error("Error creating PDF", error);
+    }
 }
 
 generatePDF();
